39fffeeee1131d704645eec1531ab19023160f9e
Ahmed Muhammad <ahmedm357@gmail.com>
2021-11-11 23:12:17 -0500

    reviewed ddl and dml

--- sql/insert.sql
+++ sql/insert.sql
@@ -1,19 +1,24 @@
+-- customer table
 INSERT INTO CUSTOMER VALUES ('JD123','12345','John','Doe','456 washtenaw ave','Ann Arbor','Michigan','48198','VISA',5555555555555555,'2021-04-09',111111);
 
 INSERT INTO CUSTOMER VALUES ('BM734','54321','Brie','Macbeth','981 packard st','Ann Arbor','Michigan','48012','MASTER',4444444444444444,'2021-03-03',222222);  
 
 INSERT INTO CUSTOMER VALUES ('MJ463','71231','Miranda','Johnson','612 Orchard Rd','Ypsilanti','Michigan','48365','VISA',3333333333333333,'2023-04-09',333333);  
 
+-- book table
 INSERT INTO BOOK VALUES (1234567891234, 'Lord of the Rings', 'J.R.R. Tolkien', 'Allen & Unwin', 50.00, 'Fantasy', 7);
 
-INSERT INTO BOOK VALUES (1111111111111, 'Moby Dick', 'Herman Melville', 'Richard Bentley', 63.00, 'Fiction', 4);
+INSERT INTO BOOK VALUES (1111111111111, 'Moby Dick', 'Herman Melville', 'Richard Bentley', 63.00, 'American Literature', 4);
 
 INSERT INTO BOOK VALUES (9876543210123, 'Harry Potter', 'J.K. Rowling', 'Bloomsbury', 43.00, 'Fantasy', 10);
+
+INSERT INTO BOOK VALUES (9272523210123, 'Speaker for the Dead', 'Orson Scott Card', 'Tor Books', 10.00, 'Science Fiction', 10);
 
 INSERT INTO BOOK VALUES (2222222222222, 'Zen and the Art of Motorcycle Maintenance: An Inquiry Into VaLues', 'Robert M. Pirsig', 'William Morrow', 43.00, 'Adventure', 10);
 
 INSERT INTO BOOK VALUES (9999999999999, 'The Dunwich Horror', 'H.P. Lovecraft', 'Weird Tales', 5.99, 'Horror', 1);
 
+-- review table
 INSERT INTO REVIEW VALUES (123456,'fantastic book would recommend!',1111111111111);
 
 INSERT INTO REVIEW VALUES (654321,'something for nerds, it was ok',9876543210123);
