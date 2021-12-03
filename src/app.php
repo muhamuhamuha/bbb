e1a8a74b53854185e944df9f612bdb0548151201
Ahmed Muhammad <ahmedm357@gmail.com>
2021-12-03 17:49:28 -0500

    moved calcSubtotal and calcShipping functions here

--- src/app.php
+++ src/app.php
@@ -14,6 +14,37 @@
 		return true;
 	}
 
-     
+	/**
+	 * multiplies all the quantities in the array of records
+	 * after summing them, by 2.
+	 */
+	function calcShipping(array $books, string $quantityCol = 'Quantity'): int {
+		// filter out quantites and cast
+		$quantities = array_map(function($x) use ($quantityCol) { return intval($x[$quantityCol]); },
+														$books);
+
+		return array_sum($quantities) * 2;
+	}
+
+  /**
+	 * Given an array of records with a price field and quantity field,
+	 * this function will output the subtotal after casting the data to
+	 * the correct type.
+	 */ 
+	function calcSubtotal(array $books,
+												string $priceCol = 'Price',
+												string $quantityCol = 'Quantity'): float {
+		// filter out prices and cast
+		$prices = array_map(function($x) use ($priceCol) { return floatval($x[$priceCol]); },
+												$books);
+		// filter out quantites and cast
+		$quantities = array_map(function($x) use ($quantityCol) { return intval($x[$quantityCol]); },
+														$books);
+
+		for ($i = 0; $i < count($prices); $i++) {
+			$prices[$i] = $quantities[$i] * $prices[$i];
+		}
+		return array_sum($prices);
+	}
 
 ?>
