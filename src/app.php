3870d4ba0ef5e640e2fa79be7cb53ec8493db658
Ahmed Muhammad <ahmedm357@gmail.com>
2021-12-03 00:46:49 -0500

    moved assert function from customer_registration to here

--- src/app.php
+++ src/app.php
@@ -1,8 +1,19 @@
 <?php
 
-function validate_request(string $key): bool {
-    $_REQUEST[$key]
+    /**
+     * Given an array (like a $_POST), will ensure all items in the
+     * array are not empty.
+     */
+	function assert_no_empty_fields(array $keys): bool {
+		foreach($keys as $k => $v) {
+			if ( empty($v) ) {
+				raise_alert("$k cannot be empty.");
+				return false;
+			}
+		}
+		return true;
+	}
 
-}
+     
 
 ?>
