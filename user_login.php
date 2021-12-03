86470c5adfbd989e3225716846d930bfb0b5c2cf
Ahmed Muhammad <ahmedm357@gmail.com>
2021-12-03 12:07:24 -0500

    working

--- user_login.php
+++ user_login.php
@@ -1,11 +1,11 @@
-
+<?php session_start(); ?>
 <!DOCTYPE HTML>
 <head>
 <title>User Login</title>
 </head>
 <body>
 	<table align="center" style="border:2px solid blue;">
-		<form action="screen2.php" method="post" id="login_screen">
+		<form action="" method="post" id="login_screen">
 		<tr>
 			<td align="right">
 				Username<span style="color:red">*</span>:
@@ -33,5 +33,26 @@
 		</tr>
 	</table>
 </body>
-
 </html>
+<?php
+	require_once __DIR__ . '/src/db.php';
+
+	if (checkRequest()) {
+		$user = "'" . $_POST['username'] . "'";
+		$pin = "'" . $_POST['pin'] . "'";
+
+		$query = "SELECT FirstName, LastName FROM CUSTOMER ";
+		$res = db\select_from_db($query . "WHERE Username = $user AND PIN = $pin;");
+
+		if (empty($res)) {
+			raise_alert("Sorry, that information doesn't line up with what we have in the system.");
+		} else {
+			[$fname, $lname] = array_values($res[0]);
+			$_SESSION['username'] = $user;
+			raise_alert("Welcome back $fname $lname");
+			header('Refresh: 0; url=screen2.php');
+			
+		}
+
+	}
+?>
